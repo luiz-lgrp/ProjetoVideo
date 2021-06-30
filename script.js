fc52681a05f7aa22cc3b32e9a02e02dd764ab423
Gustavo Luiz <70780066+luiz-lgrp@users.noreply.github.com>
2021-06-30 10:16:01 -0300

    update file

---
+++ script.js
@@ -0,0 +1,18 @@
+let video = document.getElementById("video1");
+
+function play() {
+  video.play();
+}
+
+function pausar() {
+  video.pause();
+}
+
+function parar() {
+  video.pause();
+  video.currentTime = 0;
+}
+
+
+
+
